c9199adff2fcf2ceb9da9018ce3d98e7d4fd0204
Ethel Morgan <eth@ethulhu.co.uk>
2020-02-06 14:14:26 +0000

    add UPnP AV RenderingControl stub

---
+++ upnpav/renderingcontrol/api.go
@@ -0,0 +1,8 @@
+package renderingcontrol
+
+import "github.com/ethulhu/helix/upnp/ssdp"
+
+const (
+	Version1 = ssdp.URN("urn:schemas-upnp-org:service:RenderingControl:1")
+	Version2 = ssdp.URN("urn:schemas-upnp-org:service:RenderingControl:2")
+)

8ff31f6edc00563516d3bfdbba1c211fd4fe01ac
Ethel Morgan <eth@ethulhu.co.uk>
2020-05-02 12:38:12 +0100

    add basic transports control UI to cmd/helix-player

--- cmd/helix-player/static/api.js
+++ cmd/helix-player/static/api.js
@@ -5,6 +5,19 @@ async function getJSON( url ) {
 	return rsp.json();
 }
 
+async function postForm( url, data ) {
+	let fd = new FormData();
+	Object.entries( data ).forEach( ( [ k, v ] ) => {
+		fd.append( k, v );
+	} );
+	return fetch( url, {
+		method: 'POST',
+		body: fd,
+	} );
+}
+
+// ContentDirectory APIs.
+
 export async function fetchDirectories() {
 	return getJSON( '/directories/' );
 }
@@ -14,3 +27,26 @@ export async function fetchObject( directory, id ) {
 }
 
 export const rootObject = '0';
+
+
+// AVTransport APIs.
+
+export async function fetchTransports() {
+	return getJSON( '/transports/' );
+}
+
+export async function fetchTransport( udn ) {
+	return getJSON( `/transports/${udn}` );
+}
+
+export async function playTransport( udn ) {
+	return postForm( `/transports/${udn}`, { action: 'play' } );
+}
+
+export async function pauseTransport( udn ) {
+	return postForm( `/transports/${udn}`, { action: 'pause' } );
+}
+
+export async function stopTransport( udn ) {
+	return postForm( `/transports/${udn}`, { action: 'stop' } );
+}

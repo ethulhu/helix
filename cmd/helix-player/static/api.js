47e0d7fb02248c5ece360273bfbcb3ac2cefd8ba
Ethel Morgan <eth@ethulhu.co.uk>
2020-05-02 10:40:54 +0100

    DRY JSON fetch() to getJSON for cmd/helix-player api.js

--- cmd/helix-player/static/api.js
+++ cmd/helix-player/static/api.js
@@ -1,15 +1,16 @@
-export async function fetchDirectories() {
-	const rsp = await fetch( '/directories/', {
+async function getJSON( url ) {
+	const rsp = await fetch( url, {
 		headers: { Accept: 'application/json' },
 	} );
-	return rsp.json()
+	return rsp.json();
+}
+
+export async function fetchDirectories() {
+	return getJSON( '/directories/' );
 }
 
 export async function fetchObject( directory, id ) {
-	const rsp = await fetch( `/directories/${directory}/${id}`, {
-		headers: { Accept: 'application/json' },
-	} );
-	return rsp.json()
+	return getJSON( `/directories/${directory}/${id}` );
 }
 
 export const rootObject = '0';

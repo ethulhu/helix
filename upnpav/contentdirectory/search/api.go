d1db817ae0a4040925966a10a53967c728548434
Ethel Morgan <eth@ethulhu.co.uk>
2020-05-08 13:57:05 +0100

    add documentation to upnpav/contentdirectory/search

--- upnpav/contentdirectory/search/api.go
+++ upnpav/contentdirectory/search/api.go
@@ -1,3 +1,4 @@
+// Package search implements the DLNA query language, defined in the ContentDirectory spec.
 package search
 
 import (
@@ -7,34 +8,46 @@ import (
 )
 
 type (
+	// Criteria is a `searchCrit` from the ContentDirectory specification.
 	Criteria interface {
 		Criteria() Criteria
 		String() string
 	}
 
+	// Everything matches everything, a.k.a. "*".
 	Everything struct{}
 
+	// Query wraps some query expression.
 	Query struct {
 		Expr
 	}
 
+	// Expr is a query or subquery expression.
 	Expr interface {
 		CanonicalExpr() Expr
 		String() string
 	}
 
-	LogicOp   string
+	// LogicOp is a logical operation, like `&&` or `||`.
+	LogicOp string
+
+	// LogicExpr is the conjunction or disjunction of one or more subquery Exprs.
 	LogicExpr struct {
 		Op       LogicOp
 		SubExprs []Expr
 	}
 
+	// ExistsExpr matches Directory Objects that either do or don't have a given property.
 	ExistsExpr struct {
 		Property string
 		Exists   bool
 	}
 
-	BinaryOp   string
+	// BinaryOp is a binary operation on a property, such as `=` or `contains`.
+	BinaryOp string
+
+	// BinaryExpr matches Directory Objects where Property matches the relation to Operand by Op.
+	// For example, `artist = "The Birthday Massacre"`.
 	BinaryExpr struct {
 		Property string
 		Op       BinaryOp
@@ -54,7 +67,10 @@ const (
 	GreaterThanEqual = BinaryOp(">=")
 	Contains         = BinaryOp("contains")
 	DoesNotContain   = BinaryOp("doesNotContain")
-	DerivedFrom      = BinaryOp("derivedfrom")
+
+	// DerivedFrom is useful for restricting a query to only certain media kinds.
+	// For example, `itemClass derivedfrom "object.item.audioItem"`.
+	DerivedFrom = BinaryOp("derivedfrom")
 )
 
 func (e Everything) String() string {
@@ -116,6 +132,7 @@ func (b BinaryExpr) CanonicalExpr() Expr {
 	return b
 }
 
+// Parse parses a DLNA query string and returns a Criteria.
 // TODO: Support operator precidence for And and Or (And binds stronger than Or).
 func Parse(src string) (Criteria, error) {
 	tokens, err := tokenize(src)

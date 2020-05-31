a8bf0184c9dc5bf1ed4728a1c687590b5c74c073
Ethel Morgan <eth@ethulhu.co.uk>
2020-05-31 17:56:24 +0100

    fill in all possible ConnectionManager message types
    also move "comma-separated protocolinfos" to its own type,
    commaSeparatedProtocolInfos.

--- upnpav/connectionmanager/client.go
+++ upnpav/connectionmanager/client.go
@@ -6,9 +6,6 @@ package connectionmanager
 
 import (
 	"context"
-	"fmt"
-	"log"
-	"strings"
 
 	"github.com/ethulhu/helix/soap"
 	"github.com/ethulhu/helix/upnpav"
@@ -25,34 +22,8 @@ func NewClient(soapClient soap.Interface) Interface {
 func (c *client) ProtocolInfo(ctx context.Context) ([]*upnpav.ProtocolInfo, []*upnpav.ProtocolInfo, error) {
 	req := getProtocolInfoRequest{}
 	rsp := getProtocolInfoResponse{}
-	if err := c.Call(ctx, string(Version1), "GetProtocolInfo", req, &rsp); err != nil {
-		return nil, nil, fmt.Errorf("could not get protocol info: %w", err)
+	if err := c.Call(ctx, string(Version1), getProtocolInfo, req, &rsp); err != nil {
+		return nil, nil, err
 	}
-
-	var sources []*upnpav.ProtocolInfo
-	for _, source := range strings.Split(rsp.Source, ",") {
-		if source == "" {
-			continue
-		}
-		protocolInfo, err := upnpav.ParseProtocolInfo(source)
-		if err != nil {
-			// TODO: do something proper here.
-			log.Printf("o no: %v", err)
-		}
-		sources = append(sources, protocolInfo)
-	}
-
-	var sinks []*upnpav.ProtocolInfo
-	for _, sink := range strings.Split(rsp.Sink, ",") {
-		if sink == "" {
-			continue
-		}
-		protocolInfo, err := upnpav.ParseProtocolInfo(sink)
-		if err != nil {
-			// TODO: do something proper here.
-			log.Printf("o no: %v", err)
-		}
-		sinks = append(sinks, protocolInfo)
-	}
-	return sources, sinks, nil
+	return rsp.Sources, rsp.Sinks, nil
 }
--- upnpav/connectionmanager/messages.go
+++ upnpav/connectionmanager/messages.go
@@ -4,15 +4,123 @@
 
 package connectionmanager
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+	"fmt"
+	"strings"
+
+	"github.com/ethulhu/helix/upnpav"
+)
 
 type (
+	commaSeparatedProtocolInfos []*upnpav.ProtocolInfo
+	commaSeparatedInts          []int
+
+	direction string
+	status    string
+
 	getProtocolInfoRequest struct {
 		XMLName xml.Name `xml:"urn:schemas-upnp-org:service:ConnectionManager:1 GetProtocolInfo"`
 	}
 	getProtocolInfoResponse struct {
-		XMLName xml.Name `xml:"urn:schemas-upnp-org:service:ConnectionManager:1 GetProtocolInfoResponse"`
-		Source  string   `xml:"Source"`
-		Sink    string   `xml:"Sink"`
+		XMLName xml.Name                    `xml:"urn:schemas-upnp-org:service:ConnectionManager:1 GetProtocolInfoResponse"`
+		Sources commaSeparatedProtocolInfos `xml:"Source"`
+		Sinks   commaSeparatedProtocolInfos `xml:"Sink"`
+	}
+
+	prepareForConnectionRequest struct {
+		XMLName               xml.Name  `xml:"urn:schemas-upnp-org:service:ConnectionManager:1 PrepareForConnection"`
+		RemoteProtocolInfo    string    `xml:"RemoteProtocolInfo"`
+		PeerConnectionManager string    `xml:"PeerConnectionManager"`
+		PeerConnectionID      int       `xml:"PeerConnectionID"`
+		Direction             direction `xml:"Direction"`
+	}
+	prepareForConnectionResponse struct {
+		XMLName       xml.Name `xml:"urn:schemas-upnp-org:service:ConnectionManager:1 PrepareForConnectionResponse"`
+		ConnectionID  int      `xml:"ConnectionID"`
+		AVTransportID int      `xml:"AVTransportID"`
+		ResID         int      `xml:"ResID"`
+	}
+
+	connectionCompleteRequest struct {
+		XMLName      xml.Name `xml:"urn:schemas-upnp-org:service:ConnectionManager:1 ConnectionComplete"`
+		ConnectionID int      `xml:"ConnectionID"`
+	}
+	connectionCompleteResponse struct {
+		XMLName xml.Name `xml:"urn:schemas-upnp-org:service:ConnectionManager:1 ConnectionCompleteResponse"`
+	}
+
+	getCurrentConnectionIDsRequest struct {
+		XMLName xml.Name `xml:"urn:schemas-upnp-org:service:ConnectionManager:1 GetCurrentConnectionIDs"`
+	}
+	getCurrentConnectionIDsResponse struct {
+		XMLName       xml.Name           `xml:"urn:schemas-upnp-org:service:ConnectionManager:1 GetCurrentConnectionIDsResponse"`
+		ConnectionIDs commaSeparatedInts `xml:"ConnectionIDs"`
+	}
+
+	getCurrentConnectionInfoRequest struct {
+		XMLName      xml.Name `xml:"urn:schemas-upnp-org:service:ConnectionManager:1 GetCurrentConnectionInfo"`
+		ConnectionID int      `xml:"ConnectionID"`
+	}
+	getCurrentConnectionInfoResponse struct {
+		XMLName               xml.Name  `xml:"urn:schemas-upnp-org:service:ConnectionManager:1 GetCurrentConnectionInfoResponse"`
+		AVTransportID         int       `xml:"AVTransportID"`
+		ResID                 int       `xml:"ResID"`
+		ProtocolInfo          string    `xml:"ProtocolInfo"`
+		PeerConnecitonManager string    `xml:"PeerConnecitonManager"`
+		PeerConnecitonID      int       `xml:"PeerConnecitonID"`
+		Direction             direction `xml:"Direction"`
+		Status                status    `xml:"Status"`
 	}
 )
+
+const (
+	input  = direction("Input")
+	output = direction("Output")
+)
+
+const (
+	ok                    = status("OK")
+	contentFormatMismatch = status("ContentFormatMismatch")
+	insufficientBandwidth = status("InsufficientBandwidth")
+	unreliableChannel     = status("UnreliableChannel")
+	unknown               = status("Unknown")
+)
+
+const (
+	getProtocolInfo          = "GetProtocolInfo"
+	prepareForConnection     = "PrepareForConnection"
+	connectionComplete       = "ConnectionComplete"
+	getCurrentConnectionIDs  = "GetCurrentConnectionIDs"
+	getCurrentConnectionInfo = "GetCurrentConnectionInfo"
+)
+
+func (ps commaSeparatedProtocolInfos) MarshalXML(e *xml.Encoder, el xml.StartElement) error {
+	var piStrings []string
+	for _, p := range ps {
+		piStrings = append(piStrings, p.String())
+	}
+	return e.EncodeElement(strings.Join(piStrings, ","), el)
+}
+
+func (ps *commaSeparatedProtocolInfos) UnmarshalXML(d *xml.Decoder, el xml.StartElement) error {
+	var s string
+	if err := d.DecodeElement(&s, &el); err != nil {
+		return err
+	}
+	if s == "" {
+		return nil
+	}
+
+	var protocolInfos []*upnpav.ProtocolInfo
+	for _, p := range strings.Split(s, ",") {
+		protocolInfo, err := upnpav.ParseProtocolInfo(p)
+		if err != nil {
+			return fmt.Errorf("could not parse ProtocolInfo %q: %v", p, err)
+		}
+		protocolInfos = append(protocolInfos, protocolInfo)
+	}
+
+	*ps = protocolInfos
+	return nil
+}

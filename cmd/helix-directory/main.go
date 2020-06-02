cadc02682076d533fa7d36e18a6b5bbc866163a5
Ethel Morgan <eth@ethulhu.co.uk>
2020-06-02 09:46:05 +0100

    very basic ContentDirectory server works
    I can browse it with helix-player and play files from it with upmpdcli.

--- cmd/helix-directory/main.go
+++ cmd/helix-directory/main.go
@@ -9,10 +9,13 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"mime"
 	"net"
 	"net/http"
+	"net/url"
 	"os"
 	"path"
+	"strings"
 
 	"github.com/ethulhu/helix/flag"
 	"github.com/ethulhu/helix/upnp"
@@ -25,7 +28,12 @@ import (
 )
 
 var (
-	filePath = flag.String("path", "", "path to serve")
+	basePath = flag.Custom("path", "", "path to serve", func(raw string) (interface{}, error) {
+		if raw == "" {
+			return nil, errors.New("must not be empty")
+		}
+		return raw, nil
+	})
 
 	iface = flag.Custom("interface", "", "interface to listen on (will try to find a Private IPv4 if unset)", func(raw string) (interface{}, error) {
 		if raw == "" {
@@ -38,6 +46,7 @@ var (
 func main() {
 	flag.Parse()
 
+	basePath := (*basePath).(string)
 	iface := (*iface).(*net.Interface)
 
 	ip, err := suitableIP(iface)
@@ -67,21 +76,34 @@ func main() {
 		"listener": httpConn.Addr(),
 	}).Info("created HTTP listener")
 
-	d := upnp.NewDevice("Helix", "uuid:941b0ec2-aca8-4ce1-b64a-329f5762864d")
-	d.DeviceType = contentdirectory.DeviceType
-	d.Manufacturer = "Eth Morgan"
-	d.ManufacturerURL = "https://ethulhu.co.uk"
-	d.ModelDescription = "Helix"
-	d.ModelName = "Helix"
-	d.ModelNumber = "42"
-	d.ModelURL = "https://ethulhu.co.uk"
-	d.SerialNumber = "00000000"
+	device := upnp.NewDevice("Helix", "uuid:941b0ec2-aca8-4ce1-b64a-329f5762864d")
+	device.DeviceType = contentdirectory.DeviceType
+	device.Manufacturer = "Eth Morgan"
+	device.ManufacturerURL = "https://ethulhu.co.uk"
+	device.ModelDescription = "Helix"
+	device.ModelName = "Helix"
+	device.ModelNumber = "42"
+	device.ModelURL = "https://ethulhu.co.uk"
+	device.SerialNumber = "00000000"
 
-	d.Handle(contentdirectory.Version1, contentdirectory.ServiceID, contentdirectory.SCPD, contentdirectory.SOAPHandler{&contentDirectory{}})
-	d.Handle(connectionmanager.Version1, connectionmanager.ServiceID, connectionmanager.SCPD, nil)
+	cd := contentDirectory{
+		BasePath: basePath,
+		BaseURL: &url.URL{
+			Scheme: "http",
+			Host:   httpConn.Addr().String(),
+			Path:   "/objects/",
+		},
+	}
+	device.Handle(contentdirectory.Version1, contentdirectory.ServiceID, contentdirectory.SCPD, contentdirectory.SOAPHandler{&cd})
+	device.Handle(connectionmanager.Version1, connectionmanager.ServiceID, connectionmanager.SCPD, nil)
+
+	mux := http.NewServeMux()
+	mux.Handle("/objects/", http.StripPrefix("/objects/", http.FileServer(http.Dir(basePath))))
+	mux.Handle("/", device)
+
+	server := &http.Server{Handler: mux}
 
 	go func() {
-		server := &http.Server{Handler: d}
 		log.WithFields(log.Fields{
 			"listener": httpConn.Addr(),
 		}).Info("serving HTTP")
@@ -93,7 +115,7 @@ func main() {
 		}
 	}()
 
-	if err := upnp.BroadcastDevice(d, httpConn.Addr(), nil); err != nil {
+	if err := upnp.BroadcastDevice(device, httpConn.Addr(), nil); err != nil {
 		log.WithFields(log.Fields{
 			"error": err,
 		}).Fatal("could serve SSDP")
@@ -101,63 +123,113 @@ func main() {
 }
 
 type contentDirectory struct {
+	BasePath string
+	BaseURL  *url.URL
 }
 
 func (cd *contentDirectory) BrowseMetadata(_ context.Context, id upnpav.ObjectID) (*upnpav.DIDLLite, error) {
-	if id == contentdirectory.Root {
-		id = upnpav.ObjectID("")
+	fields := log.Fields{
+		"method": "BrowseMetadata",
+		"object": id,
 	}
-	p := path.Join(*filePath, string(id))
+
+	p, ok := cd.path(id)
+	if !ok {
+		log.WithFields(fields).Error("bad path")
+		return nil, contentdirectory.ErrNoSuchObject
+	}
 
 	fi, err := os.Stat(p)
-	if err != nil {
-		log.Printf("could not open %v: %v", p, err)
-		return nil, err
+	if errors.Is(err, os.ErrNotExist) {
+		log.WithFields(fields).Info("path does not exist")
+		return nil, contentdirectory.ErrNoSuchObject
 	}
-	didllite := &upnpav.DIDLLite{
-		Containers: []upnpav.Container{
-			{
-				Object: upnpav.Object{
-					ID:     id,
-					Title:  fi.Name(),
-					Date:   &upnpav.Date{fi.ModTime()},
-					Class:  upnpav.StorageFolder,
-					Parent: upnpav.ObjectID("-1"),
-				},
-			},
-		},
+	if err != nil {
+		fields["error"] = err
+		log.WithFields(fields).Warning("could not stat path")
+		return nil, upnpav.ErrActionFailed
 	}
 
-	fs, err := ioutil.ReadDir(p)
-	if err != nil {
-		log.Printf("could not open %v: %v", p, err)
-		return nil, err
+	if fi.IsDir() {
+		container, err := cd.containerFromPath(p)
+		if err != nil {
+			fields["error"] = err
+			log.WithFields(fields).Warning("could not describe container from path")
+			return nil, upnpav.ErrActionFailed
+		}
+		return &upnpav.DIDLLite{Containers: []upnpav.Container{container}}, nil
 	}
-	didllite.Containers[0].ChildCount = len(fs)
 
-	return didllite, nil
+	item, ok, err := cd.itemFromPath(p)
+	if err != nil {
+		fields["error"] = err
+		log.WithFields(fields).Warning("could not describe item from path")
+		return nil, upnpav.ErrActionFailed
+	}
+	if !ok {
+		log.WithFields(fields).Warning("item exists but is not a media item")
+		return nil, contentdirectory.ErrNoSuchObject
+	}
+	return &upnpav.DIDLLite{Items: []upnpav.Item{item}}, nil
 }
 func (cd *contentDirectory) BrowseChildren(_ context.Context, parent upnpav.ObjectID) (*upnpav.DIDLLite, error) {
-	if parent == contentdirectory.Root {
-		parent = upnpav.ObjectID("")
+	fields := log.Fields{
+		"method": "BrowseChildren",
+		"object": parent,
 	}
-	p := path.Join(*filePath, string(parent))
+
+	p, ok := cd.path(parent)
+	if !ok {
+		log.WithFields(fields).Error("bad path")
+		return nil, contentdirectory.ErrNoSuchObject
+	}
+
+	fi, err := os.Stat(p)
+	if errors.Is(err, os.ErrNotExist) {
+		log.WithFields(fields).Info("path does not exist")
+		return nil, contentdirectory.ErrNoSuchObject
+	}
+	if err != nil {
+		fields["error"] = err
+		log.WithFields(fields).Warning("could not stat path")
+		return nil, upnpav.ErrActionFailed
+	}
+
+	if !fi.IsDir() {
+		log.WithFields(fields).Info("not a directory")
+		return nil, nil
+	}
+
+	didllite := &upnpav.DIDLLite{}
+
 	fs, err := ioutil.ReadDir(p)
 	if err != nil {
-		log.Printf("could not open %v: %v", p, err)
-		return nil, err
+		fields["error"] = err
+		log.WithFields(fields).Error("could not list directory")
+		return didllite, upnpav.ErrActionFailed
 	}
-	didllite := &upnpav.DIDLLite{}
+
 	for _, fi := range fs {
-		didllite.Containers = append(didllite.Containers, upnpav.Container{
-			Object: upnpav.Object{
-				ID:     upnpav.ObjectID(path.Join(*filePath, string(parent), fi.Name())),
-				Date:   &upnpav.Date{fi.ModTime()},
-				Title:  fi.Name(),
-				Class:  upnpav.StorageFolder,
-				Parent: parent,
-			},
-		})
+		if fi.IsDir() {
+			container, err := cd.containerFromPath(path.Join(p, fi.Name()))
+			if err != nil {
+				fields["error"] = err
+				log.WithFields(fields).Warning("could not create container from path")
+				continue
+			}
+			didllite.Containers = append(didllite.Containers, container)
+		} else {
+			item, ok, err := cd.itemFromPath(path.Join(p, fi.Name()))
+			if err != nil {
+				fields["error"] = err
+				log.WithFields(fields).Warning("could not create item from path")
+				continue
+			}
+			if !ok {
+				continue
+			}
+			didllite.Items = append(didllite.Items, item)
+		}
 	}
 	return didllite, nil
 }
@@ -171,16 +243,7 @@ func (cd *contentDirectory) SystemUpdateID(_ context.Context) (uint, error) {
 	return 0, nil
 }
 func (cd *contentDirectory) Search(_ context.Context, _ upnpav.ObjectID, _ search.Criteria) (*upnpav.DIDLLite, error) {
-	return &upnpav.DIDLLite{
-		Items: []upnpav.Item{
-			{
-				Object: upnpav.Object{
-					Title: "hello",
-					Class: upnpav.AudioItem,
-				},
-			},
-		},
-	}, nil
+	return nil, nil
 }
 
 func suitableIP(iface *net.Interface) (net.IP, error) {
@@ -215,4 +278,76 @@ func suitableIP(iface *net.Interface) (net.IP, error) {
 		return ip, nil
 	}
 	return nil, err
+}
+
+func (cd *contentDirectory) path(id upnpav.ObjectID) (string, bool) {
+	if id == contentdirectory.Root {
+		return cd.BasePath, true
+	}
+
+	maybePath := path.Clean(path.Join(cd.BasePath, string(id)))
+	if strings.HasPrefix(maybePath, ".") || strings.HasPrefix(maybePath, "/") {
+		return "", false
+	}
+	return maybePath, true
+}
+
+func (cd *contentDirectory) containerFromPath(p string) (upnpav.Container, error) {
+	container := upnpav.Container{
+		Object: upnpav.Object{
+			ID:     upnpav.ObjectID(p),
+			Class:  upnpav.StorageFolder,
+			Parent: upnpav.ObjectID(path.Dir(p)), // TODO: wat.
+			// Parent: upnpav.ObjectID("-1"),
+		},
+	}
+	fi, err := os.Stat(p)
+	if err != nil {
+		return container, err
+	}
+	container.Title = fi.Name()
+	container.Date = &upnpav.Date{fi.ModTime()}
+
+	fs, err := ioutil.ReadDir(p)
+	if err != nil {
+		return container, err
+	}
+	container.ChildCount = len(fs)
+
+	return container, nil
+}
+
+func (cd *contentDirectory) itemFromPath(p string) (upnpav.Item, bool, error) {
+	var class upnpav.Class
+	mimetype := mime.TypeByExtension(path.Ext(p))
+	switch strings.Split(mimetype, "/")[0] {
+	case "audio":
+		class = upnpav.AudioItem
+	case "video":
+		class = upnpav.VideoItem
+	default:
+		return upnpav.Item{}, false, nil
+	}
+
+	uri := *(cd.BaseURL)
+	uri.Path = path.Join(uri.Path, p)
+
+	item := upnpav.Item{
+		Object: upnpav.Object{
+			ID:    upnpav.ObjectID(p),
+			Class: class,
+			Title: path.Base(p),
+		},
+		Resources: []upnpav.Resource{{
+			URI: (&uri).String(),
+			ProtocolInfo: &upnpav.ProtocolInfo{
+				Protocol:      upnpav.ProtocolHTTP,
+				ContentFormat: mimetype,
+			},
+		}},
+	}
+
+	// TODO: something with ffprobe, probably.
+
+	return item, true, nil
 }
